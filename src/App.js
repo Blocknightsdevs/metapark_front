c4db80539613945f2f83eee3504a21589527f918
rodrigogk87 <88176133+Blocknightsdevs@users.noreply.github.com>
2021-11-29 00:07:22 -0300

    some fixes, added timestamps, but and claim

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ import Web3 from "web3";
 import * as ether_utils from "./ether-utils.json";
 import * as errors from "./errors.json";
 
-const mPARKToken="0x4631BCAbD6dF18D94796344963cB60d44a4136b6";
-const presale="0x86A2EE8FAf9A840F7a2c64CA3d51209F9A02081D"; 
+const mPARKToken="0x4b6aB5F819A515382B0dEB6935D793817bB4af28";
+const presale="0xCace1b78160AE76398F486c8a18044da0d66d86D";
+
 function App() {
   const [web3, setWeb3] = useState(undefined);
   const [accounts, setAccounts] = useState(undefined);
@@ -17,6 +18,8 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [networkId, setNetworkId] = useState(undefined);
   const [error, setError] = useState(false);
+  const [endTime, setEndTime] = useState("");
+  const [presaleEnded, setPresaleEnded] = useState(true);
 
   function checkNewtwork(networkId) {
     //check network
@@ -45,16 +48,24 @@ function App() {
           mPARKToken
         );
 
-        let contribution = await presaleContractInterface.methods.checkContribution(accounts[0]).call();
-        console.log(contribution,"checkContribution");
+        let contribution = await presaleContractInterface.methods
+          .checkContribution(accounts[0])
+          .call();
+        console.log(contribution, "checkContribution");
 
-        let balanceOfPresale = await tokenContractInterface.methods.balanceOf(accounts[0]).call();
-        console.log(balanceOfPresale,"balanceOf");
+        let balanceOfPresale = await tokenContractInterface.methods
+          .balanceOf(accounts[0])
+          .call();
+        console.log(balanceOfPresale, "balanceOf");
 
-
-        let blockTime = await presaleContractInterface.methods.getTimeStamp().call();
-        let endTime = await presaleContractInterface.methods.getEndDate().call();
-        console.log(blockTime,endTime,"blockTime and endtime");
+        let blockTime = await presaleContractInterface.methods
+          .getTimeStamp()
+          .call();
+        let endTime = await presaleContractInterface.methods
+          .getEndDate()
+          .call();
+        //console.log(blockTime,endTime,"blockTime and endtime");
+        getTimestapedDates(endTime);
 
         setContract(presaleContractInterface);
         setWeb3(web3);
@@ -86,28 +97,56 @@ function App() {
     }
   }, []);
 
-  
-  useEffect(() => {},[]);
+  useEffect(() => {}, [endTime]);
 
+  function getTimestapedDates(timestamp) {
+    var d1 = new Date();
+    // Create a new JavaScript Date object based on the timestamp
+    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+    var date = new Date(timestamp * 1000);
+    // Hours part from the timestamp
+    var hours = date.getHours();
+    // Minutes part from the timestamp
+    var minutes = "0" + date.getMinutes();
+    // Seconds part from the timestamp
+    var seconds = "0" + date.getSeconds();
 
+    // Will display time in 10:30:23 format
+    var formattedTime =
+      date.toDateString() +
+      ", " +
+      hours +
+      ":" +
+      minutes.substr(-2) +
+      ":" +
+      seconds.substr(-2);
+
+    if (d1 > date) {
+      setPresaleEnded(true);
+    } else {
+      setPresaleEnded(false);
+      setEndTime(formattedTime);
+    }
+  }
 
   const buyTokens = async () => {
-    let amount = web3.utils.toWei("0.1",'ether');
-    await contract.methods.buyTokens(accounts[0]).send({from:accounts[0],value:amount});
-  }
+    let amount = web3.utils.toWei("0.1", "ether");
+    await contract.methods
+      .buyTokens(accounts[0])
+      .send({ from: accounts[0], value: amount });
+  };
 
-  const claimTokens= async () => {
-    await contract.methods.claimTokens().send({from:accounts[0]});
-  }
-
-
+  const claimTokens = async () => {
+    await contract.methods.claimTokens().send({ from: accounts[0] });
+  };
 
   return (
     <>
       <div> Presale MetaPark </div>
-      <button onClick={()=> buyTokens()}>Buy Tokens</button>
       
-      <button onClick={()=> claimTokens()}>Claim Tokens</button>
+      <div>{presaleEnded ? <p>Presale ended</p> : <p>End time {endTime}</p>}</div>
+      {!presaleEnded && <button onClick={() => buyTokens()}>Buy Tokens</button> }
+      <button onClick={() => claimTokens()}>Claim Tokens</button>
     </>
   );
 }
